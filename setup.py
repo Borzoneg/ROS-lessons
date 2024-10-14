cf46cead0cf7ee45115d1b8e5e67104e35254aca
gubo <gubo@mmmi.sdu.dk>
2024-10-14 15:59:53 +0200

    Rviz and vsualizatio through urdf

--- setup.py
+++ setup.py
@@ -4,14 +4,21 @@ from setuptools import find_packages, setup
 
 package_name = 'ros_lessons'
 
+data_files=[
+            ('share/ament_index/resource_index/packages', ['resource/' + package_name]), ('share/' + package_name, ['package.xml']),
+            (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')), # added for launch files
+            (os.path.join('share', package_name, 'config'), glob('config/*')), # added for config files
+        ]
+for robot_name in os.listdir('robots'): # to extract all the robots and placed them nicely in the install folder
+    data_files.append((os.path.join('share', package_name, robot_name, 'meshes/collision'), glob(('robots/' + robot_name + '/meshes/collision/*'))))
+    data_files.append((os.path.join('share', package_name, robot_name, 'meshes/visual'),    glob(('robots/' + robot_name + '/meshes/visual/*'  ))))
+    data_files.append((os.path.join('share', package_name, robot_name, 'urdf'),             glob(('robots/' + robot_name + '/urdf/*'  ))))
+
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]), ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')), # added for launch files
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='gu',
